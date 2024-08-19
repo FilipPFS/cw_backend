848bc70e52654ff7a9dd843ba2f297685ffb3f9f
Filip Petrovic <filippetrovicfr04@gmail.com>
2024-08-19 18:32:34 +0200

    Server initialization

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, {
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from "express";
+
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.log("Connexion à MongoDB échouée !", err));
+
+app.use("/images", express.static(path.join(__dirname, "../images")));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(5000);
